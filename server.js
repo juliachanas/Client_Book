148dfc43f533526f2563c214f8d1a99684df2e41
Julia Chanas <juulia.chanas@gmail.com>
2024-03-12 18:19:46 +0100

    Prepare project to publication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db.js');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -17,10 +18,17 @@ app.use('/api', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
