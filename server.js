6dec69946797151691a4d0acc8cdda7db5e214a8
Julia Chanas <juulia.chanas@gmail.com>
2024-03-12 14:05:33 +0100

    Add endpoints for concerts

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db.js');
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
@@ -8,29 +9,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const db = [
-  { id: '1', author: 'John Doe', text: 'This company is worth every coin!' },
-  {
-    id: '2',
-    author: 'Amanda Doe',
-    text: 'They really know how to make you happy.',
-  },
-];
-
-//endpoints
+//endpoints testimonials
 
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * db.length);
-  const randomTestimonial = db[randomIndex];
+  const randomIndex = Math.floor(Math.random() * db.testimonials.length);
+  const randomTestimonial = db.testimonials[randomIndex];
   res.json(randomTestimonial);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  const testimonial = db.find((item) => item.id === req.params.id);
+  const testimonial = db.testimonials.find((item) => item.id === req.params.id);
   if (testimonial) {
     res.json(testimonial);
   } else {
@@ -50,7 +42,7 @@ app.post('/testimonials', (req, res) => {
     text,
   };
 
-  db.push(newTestimonial);
+  db.testimonials.push(newTestimonial);
   res.status(202).json({
     message: 'Testimonial added successfully',
     testimonial: newTestimonial,
@@ -61,7 +53,7 @@ app.put('/testimonials/:id', (req, res) => {
   const testimonialId = req.params.id;
   const { author, text } = req.body;
 
-  const testimonial = db.find((item) => item.id === testimonialId);
+  const testimonial = db.testimonials.find((item) => item.id === testimonialId);
 
   if (!testimonial) {
     return res.status(404).json({ error: 'Testimonial not found' });
@@ -81,17 +73,98 @@ app.put('/testimonials/:id', (req, res) => {
 app.delete('/testimonials/:id', (req, res) => {
   const testimonialId = req.params.id;
 
-  const testimonial = db.find((item) => item.id === testimonialId);
+  const testimonial = db.testimonials.find((item) => item.id === testimonialId);
 
   if (!testimonial) {
     return res.status(404).json({ error: 'Testimonial not found' });
   }
 
   // Remove the testimonial from the array
-  const index = db.indexOf(testimonial);
-  db.splice(index, 1);
+  const index = db.testimonials.indexOf(testimonial);
+  db.testimonials.splice(index, 1);
 
   return res.status(202).json({ message: 'Testimonial deleted successfully' });
+});
+
+// endpoints concerts
+
+app.get('/concerts', (req, res) => {
+  res.json(db.concerts);
+});
+
+app.get('/concerts/:id', (req, res) => {
+  const concert = db.concerts.find((item) => item.id === req.params.id);
+  if (concert) {
+    res.json(concert);
+  } else {
+    res.status(404).json({ error: 'Concert not found' });
+  }
+});
+
+app.post('/concerts', (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  if (!performer || !genre || !price || !day) {
+    return res
+      .status(404)
+      .json({ error: 'Performer, genre, price and day are required' });
+  }
+
+  const newConcert = {
+    id: uuidv4(),
+    performer,
+    genre,
+    price,
+    day,
+    image,
+  };
+
+  db.concerts.push(newConcert);
+  res.status(202).json({
+    message: 'Concert added successfully',
+    concert: newConcert,
+  });
+});
+
+app.delete('/concerts/:id', (req, res) => {
+  const concertId = req.params.id;
+
+  const concert = db.concerts.find((item) => item.id === concertId);
+
+  if (!concert) {
+    return res.status(404).json({ error: 'Concert not found' });
+  }
+
+  // Remove the concert from the array
+  const index = db.concerts.indexOf(concert);
+  db.concerts.splice(index, 1);
+
+  return res.status(202).json({ message: 'Concert deleted successfully' });
+});
+
+app.put('/concerts/:id', (req, res) => {
+  const concertId = req.params.id;
+  const { performer, genre, price, day, image } = req.body;
+
+  const concert = db.concerts.find((item) => item.id === concertId);
+
+  if (!concert) {
+    return res.status(404).json({ error: 'Concert not found' });
+  }
+
+  if (performer && genre && price && day && image) {
+    concert.performer = performer;
+    concert.genre = genre;
+    concert.price = price;
+    concert.day = day;
+    concert.image = image;
+    return res
+      .status(202)
+      .json({ message: 'Concert updated successfully', concert });
+  } else {
+    return res
+      .status(404)
+      .json({ error: 'Performer, genre, price and day are required' });
+  }
 });
 
 app.use((req, res) => {
